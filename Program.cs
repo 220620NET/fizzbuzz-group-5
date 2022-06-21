81636aa3b8b1bad6bc7872760cda38efddeef48a
Elizabeth Kang <liz.kang@hey.com>
2022-06-21 17:18:31 -0500

    Restyled to match Microsoft's C# Style Guide

--- Program.cs
+++ Program.cs
@@ -5,15 +5,21 @@
 Console.WriteLine("What is the number you'd like me to interate to?");
 int num = int.Parse(Console.ReadLine());
 
-
 for (int i = 1; i <= num; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
         Console.WriteLine("fizzbuzz");
-    } else if (i % 3 == 0) {
+    }
+    else if (i % 3 == 0)
+    {
         Console.WriteLine("fizz");
-    } else if (i % 5 == 0) {
+    }
+    else if (i % 5 == 0)
+    {
         Console.WriteLine("buzz");
-    } else {
+    }
+    else
+    {
         Console.WriteLine(i);
     }
 }
