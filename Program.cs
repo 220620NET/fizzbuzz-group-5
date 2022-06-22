2307a64ee5c66aa95e9f4072313ef83754a29e97
Elizabeth Kang <liz.kang@hey.com>
2022-06-22 11:28:45 -0500

    Added more functionality

--- Program.cs
+++ Program.cs
@@ -2,24 +2,53 @@
 //5 ---> buzz
 //3 and 5 ---> fizzbuzz
 
-Console.WriteLine("What is the number you'd like me to iterate to?");
-int num = int.Parse(Console.ReadLine());
+int num = 0;
 
-for (int i = 1; i <= num; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0))
+bool x = true;
+
+while (x)
+{
+    Console.WriteLine("What is the number you'd like me to iterate to?");
+    string userInput = Console.ReadLine();
+    Console.WriteLine("");
+    try
     {
-        Console.WriteLine("fizzbuzz");
+        num = int.Parse(userInput);
+        Console.WriteLine("Here is the iteration:");
+        for (int i = 1; i <= num; i++)
+        {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Would you like me to iternate again? (yes/no)");
+        string again = Console.ReadLine();
+        Console.WriteLine("");
+        again = again.ToLower();
+        if (!again.Equals("yes")) {
+            x = false;
+        }
+        
     }
-    else if (i % 3 == 0)
+    
+    catch (FormatException)
     {
-        Console.WriteLine("fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine("buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
+        Console.WriteLine("Not a valid input. Please type a positive integer.");
+        
     }
 }
+
